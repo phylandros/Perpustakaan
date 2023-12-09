36c2e1524e6961085ae335bebf16d0d36bf251d5
phylandros <48447281+phylandros@users.noreply.github.com>
2023-12-09 15:28:49 +0700

    Update HomeActivity dan ProfileFragment

--- app/src/main/java/com/example/perpustakaan/ProfileFragment.java
+++ app/src/main/java/com/example/perpustakaan/ProfileFragment.java
@@ -1,12 +1,16 @@
 package com.example.perpustakaan;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +62,19 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        LinearLayout toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Kembali ke HomeActivity
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        return view;
     }
 }
